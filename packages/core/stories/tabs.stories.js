6c0c5d137439ac1d74c6741bae441fe74c992ef9
Adam Raider <adam.raider@wework.com>
2019-07-18 15:57:38 -0400

    wip

--- packages/core/stories/tabs.stories.js
+++ packages/core/stories/tabs.stories.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 
-if (process.env.NODE_ENV === 'development') {
-  storiesOf('Tabs', module).add('default', () => (
-    <ul className="ray-tabs">
-      <li className="ray-tabs__item">Desktop</li>
-      <li className="ray-tabs__item">Tablet</li>
-      <li className="ray-tabs__item ray-tabs__item--active">Mobile</li>
-    </ul>
-  ));
-}
+storiesOf('Tabs', module).add('default', () => (
+  <ul className="ray-tabs">
+    <li className="ray-tabs__item">Desktop</li>
+    <li className="ray-tabs__item">Tablet</li>
+    <li className="ray-tabs__item ray-tabs__item--active">Mobile</li>
+    <li className="ray-tabs__item">Tablet</li>
+    <li className="ray-tabs__item">Tablet</li>
+    <li className="ray-tabs__item">Tablet</li>
+    <li className="ray-tabs__item">Tablet</li>
+    <li className="ray-tabs__item">Tablet</li>
+  </ul>
+));
